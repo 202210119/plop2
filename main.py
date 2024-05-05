0bb3d0280e52fe62ce925fdbc6ccdfdfc917fdb7
202210119 <202210119@feualabang.edu.ph>
2024-05-05 14:11:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,32 @@
 import streamlit as st
 import requests
+from nltk.tokenize import sent_tokenize
 
-# Define the API URL and authorization headers for the summarizer model
 API_URL = "https://api-inference.huggingface.co/models/Falconsai/text_summarization"
 headers = {"Authorization": "Bearer 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"}
 
-# Function to query the summarizer model with the provided text
-def query_summarizer_model(text):
-    payload = {"inputs": text}
+def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
-
-# Function to split the input text into sentences and summarize each
-def split_and_summarize(text):
-    # Split the input text into sentences
-    sentences = text.split(". ")
-    summaries = []
-    # Summarize each sentence
-    for sentence in sentences:
-        summary_response = query_summarizer_model(sentence)
-        if 'generated_text' in summary_response:
-            summary = summary_response['generated_text']
-            summaries.append(summary)
-    return summaries
-
-# Streamlit UI
-st.title("Sentence Summarizer")
-
-# Input text box for the essay
-essay_text = st.text_area("Input Text:", height=300)
-
-# Button to trigger sentence summarization
-if st.button("Summarize Sentences"):
-    if essay_text:
-        # Split the input text into sentences and summarize each
-        summaries = split_and_summarize(essay_text)
-        # Display the summaries
-        st.subheader("Summarized Sentences:")
-        for summary in summaries:
-            st.write(summary)
+    response_json = response.json()
+    if isinstance(response_json, list):
+        return response_json[0]["summary_text"]
     else:
-        st.warning("Please input some text first.")
+        return response_json["summary_text"][:200]
+
+def get_summary(input_text):
+    sentences = sent_tokenize(input_text)
+    summarized_text = ""
+    for sentence in sentences:
+        summarized_sentence = query({"inputs": sentence.strip()})
+        summarized_sentences = summarized_sentence.split('.')
+        for s in summarized_sentences:
+            if s.strip():
+                summarized_text += "-" + s.strip() + ".\n"
+    return summarized_text
+
+st.title("Text Summarizer")
+
+input_text = st.text_area("Enter Text:", height=300)
+if st.button("Summarize"):
+    summary = get_summary(input_text)
+    st.text_area("Summary:", value=summary, height=500)
