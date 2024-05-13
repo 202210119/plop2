c00bf47d7f5c551ad8f8ceff4cd417e56fc35024
202210119 <202210119@feualabang.edu.ph>
2024-05-13 08:10:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ if st.button("Get Notes"):
     summary = get_summary(input_text)
     st.markdown(f"<div style='white-space: pre-line; user-select: none;'>{summary}</div>", unsafe_allow_html=True)
 
-with open('./files/bg.css') as f:
+with open('bg.css') as f:
     css = f.read()
 
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
